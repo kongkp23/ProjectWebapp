b1c4146be35727732d93f8ee02adecb159fc1926
kongkp23 <gongprompong2013@gmail.com>
2025-10-15 15:19:18 +0700

    Sql

--- includes/conn.php
+++ includes/conn.php
@@ -4,7 +4,13 @@ $user = "root";
 $pass = ""; // ใส่รหัสผ่าน MySQL ของคุณ
 $db   = "fitness_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli(
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_NAME')
+);
+
 if ($conn->connect_error) {
   die("Connection  failed: " . $conn->connect_error);
 }
