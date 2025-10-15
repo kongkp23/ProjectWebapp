562242371497ada359be762fdaf6587df179f05a
kongkp23 <gongprompong2013@gmail.com>
2025-10-15 15:04:10 +0700

    submit

--- includes/conn.php
+++ includes/conn.php
@@ -6,6 +6,6 @@ $db   = "fitness_db";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Connection  failed: " . $conn->connect_error);
 }
 $conn->set_charset("utf8mb4");
